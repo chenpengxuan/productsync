47602e5213b96f05d93b60d54d353a05ee7fcc57
chenfei <chenfei@ymatou.com>
2017-02-08 14:09:27 +0800

    perfect DeleteProduct

--- domain/src/main/java/com/ymatou/productsync/domain/executor/commandconfig/DeleteProductExecutorConfig.java
+++ domain/src/main/java/com/ymatou/productsync/domain/executor/commandconfig/DeleteProductExecutorConfig.java
@@ -10,6 +10,7 @@ import com.ymatou.productsync.domain.sqlrepo.LiveCommandQuery;
 import com.ymatou.productsync.infrastructure.util.MapUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,28 +33,33 @@ public class DeleteProductExecutorConfig implements ExecutorConfig {
 
     @Override
     public List<MongoData> loadSourceData(long activityId, String productId) {
+        List<MongoData> mongoDataList = new ArrayList<>();
+
         //pc上删除商品
         if (activityId <= 0) {
             List<Map<String, Object>> deleteProducts = commandQuery.getDeleteProducts(productId);
             if (deleteProducts != null && !deleteProducts.isEmpty()) {
                 //更新商品
-                MongoDataCreator.CreateProductUpdate(MongoQueryCreator.CreateProductId(productId), deleteProducts);
+               MongoData productMd =  MongoDataCreator.CreateProductUpdate(MongoQueryCreator.CreateProductId(productId), deleteProducts);
                 //删除直播商品
                 Map<String, Object> matchConditionInfo = new HashMap();
                 matchConditionInfo.put("spid", productId);
-                //fixme:matchConditionInfo.put("end",productId); <
-                MongoDataCreator.CreateLiveProductDelete(matchConditionInfo, null);
+                //fixme:matchConditionInfo.put("end",now); <
+                MongoData liveProductMd = MongoDataCreator.CreateLiveProductDelete(matchConditionInfo, null);
+                mongoDataList.add(productMd);
+                mongoDataList.add(liveProductMd);
             }
         } else { //从直播中删除商品
             //更新直播商品
             List<Map<String, Object>> liveProducts = commandQuery.getLiveProductTime(productId, activityId);
-            MongoDataCreator.CreateLiveProductUpdate(MongoQueryCreator.CreateProductId(productId), liveProducts);
+            MongoData liveProductMd = MongoDataCreator.CreateLiveProductUpdate(MongoQueryCreator.CreateProductId(productId), liveProducts);
             //更新直播品牌
             List<Map<String, Object>> lives = liveCommandQuery.getActivityBrand(activityId);
             MapUtil.MapFieldToStringArray(lives, "brands", ",");
-            MongoDataCreator.CreateLiveProductUpdate(MongoQueryCreator.CreateLiveId(activityId), lives);
+            MongoData liveMd = MongoDataCreator.CreateLiveUpdate(MongoQueryCreator.CreateLiveId(activityId), lives);
+            mongoDataList.add(liveProductMd);
+            mongoDataList.add(liveMd);
         }
-
-        return null;
+        return mongoDataList;
     }
 }
