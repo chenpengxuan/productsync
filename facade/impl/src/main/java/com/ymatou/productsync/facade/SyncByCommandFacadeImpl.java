cd3bdd3cedf41eae59177be81e7289ccd668d9e6
zhangyong <zhangyong@ymatou.com>
2017-02-14 14:16:43 +0800

    BatchSetOnShelf,ModifyActivityPrice,SetOnShelfUpdateStockNum

--- facade/impl/src/main/java/com/ymatou/productsync/facade/SyncByCommandFacadeImpl.java
+++ facade/impl/src/main/java/com/ymatou/productsync/facade/SyncByCommandFacadeImpl.java
@@ -13,10 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -60,6 +57,25 @@ public class SyncByCommandFacadeImpl implements SyncCommandFacade {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public BaseResponse syncByCommand(SyncByCommandReq req) {
+        return getCommandResult(req);//// FIXME: 2017/2/14 需要封装成return result.IsSuccees ? "ok" : "fail";
+    }
+
+    /**
+     * Get方式供直连调用
+     *
+     * @param req 基于业务场景的请求
+     * @return
+     */
+    @GET
+    @Path("/{cache:(?i:cache)}/{mongocrud:(?i:mongocrud)}")
+    @Override
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public BaseResponse updateByCommandSync(SyncByCommandReq req) {
+        return getCommandResult(req);
+    }
+
+    private BaseResponse getCommandResult(SyncByCommandReq req) {
         ExecutorConfig config = executorConfigFactory.getCommand(req.getActionType());
         if (config == null) {
             //参数错误，无需MQ重试
